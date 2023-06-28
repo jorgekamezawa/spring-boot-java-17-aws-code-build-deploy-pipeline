d96cbbd2cc8247b3d24a58daf2db1dff83da8111
Jorge Kamezawa <jor_kamezawa@hotmail.com>
2023-06-28 12:46:30 -0300

    configuration for build with CodeBuild in S3 bucket successed

---
+++ src/main/java/com/example/helloworld/service/HelloWorldService.java
@@ -0,0 +1,12 @@
+package com.example.helloworld.service;
+
+import com.example.helloworld.model.HelloWorld;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public HelloWorld getHelloWorld() {
+        return new HelloWorld("Hello", "World");
+    }
+}
---
+++ src/test/java/com/example/helloworld/service/HelloWorldServiceTest.java
@@ -0,0 +1,29 @@
+package com.example.helloworld.service;
+
+import com.example.helloworld.model.HelloWorld;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldServiceTest {
+
+    private HelloWorldService service;
+
+    @BeforeEach
+    void setup() {
+        service = new HelloWorldService();
+    }
+
+    @Test
+    void testGetHelloWorld() {
+        // Given
+        HelloWorld expectedHelloWorld = new HelloWorld("Hello", "World");
+
+        // When
+        HelloWorld actualHelloWorld = service.getHelloWorld();
+
+        // Then
+        assertEquals(expectedHelloWorld, actualHelloWorld);
+    }
+}
