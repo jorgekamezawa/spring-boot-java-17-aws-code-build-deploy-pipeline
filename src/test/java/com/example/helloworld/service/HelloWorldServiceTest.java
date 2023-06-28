53f33071797762972345dd6e38fecf97c64e5414
Jorge Kamezawa <jor_kamezawa@hotmail.com>
2023-06-28 16:24:10 -0300

    adjusting unit tests 2

--- src/test/java/com/example/helloworld/service/HelloWorldServiceTest.java
+++ src/test/java/com/example/helloworld/service/HelloWorldServiceTest.java
@@ -18,7 +18,7 @@ class HelloWorldServiceTest {
     @Test
     void testGetHelloWorld() {
         // Given
-        HelloWorld expectedHelloWorld = new HelloWorld("Hello", "World Teste");
+        HelloWorld expectedHelloWorld = new HelloWorld("Hello", "World Teste 2");
 
         // When
         HelloWorld actualHelloWorld = service.getHelloWorld();
